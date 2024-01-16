678f4ba1f04e68d4308b409ea494ca9a79e20b64
Drakoxw <dragon12xw@gmail.com>
2024-01-16 00:56:52 -0500

    feat: se crea el package utils

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"socket/internal/app/chat"
+	"socket/internal/utils"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	
 	e.Use(middleware.CORS())
 	e.GET("/ws", chat.HandleWebSocket)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	port := utils.GetPort()
+	e.Logger.Fatal(e.Start(port))
 }
